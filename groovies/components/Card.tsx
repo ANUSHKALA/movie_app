aa07d4ad2895448934057beb12381d4881b9ad3c
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-09-02 15:15:31 +0530

    some change

--- groovies/components/Card.tsx
+++ groovies/components/Card.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
-import {inspect} from "util";
-import styles = module
+
 
 type CardProps = {
     name: String,
